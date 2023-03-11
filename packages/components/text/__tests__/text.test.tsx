c9af18c6d4442d22266107aca4cc2a62b37eb156
gimjin <17204288@qq.com>
2023-03-11 15:03:08 +0800

    test(components): [text] add line-clamp and optimize

--- packages/components/text/__tests__/text.test.tsx
+++ packages/components/text/__tests__/text.test.tsx
@@ -5,43 +5,65 @@ import Text from '../src/text.vue'
 const AXIOM = 'Rem is the best girl'
 
 describe('Text.vue', () => {
-  test('render text & class', () => {
-    const wrapper = mount(() => (
+  test('create', () => {
+    const wrapper = mount(
       <Text
         v-slots={{
           default: () => AXIOM,
         }}
       />
-    ))
-    const vm = wrapper.vm
-
-    expect(vm.$el.classList.contains('el-text')).toEqual(true)
-    expect(wrapper.text()).toEqual(AXIOM)
+    )
+    expect(wrapper.html()).toContain('el-text')
   })
 
   test('type', () => {
-    const wrapper = mount(() => <Text type="success" />)
-    const vm = wrapper.vm
-    expect(vm.$el.classList.contains('el-text--success')).toEqual(true)
+    const wrapper = mount(<Text type="success" />)
+    expect(wrapper.html()).toContain('el-text--success')
   })
 
   test('size', () => {
-    const wrapper = mount(() => <Text size="large" />)
-    const vm = wrapper.vm
-    expect(vm.$el.className.includes('el-text--large')).toEqual(true)
-    expect(vm.$el.className.includes('el-text--default')).toEqual(false)
-    expect(vm.$el.className.includes('el-text--small')).toEqual(false)
+    const wrapper = mount(<Text size="large" />)
+    expect(wrapper.html()).toContain('el-text--large')
   })
 
   test('truncated', () => {
-    const wrapper = mount(() => <Text truncated />)
-    const vm = wrapper.vm
-    expect(vm.$el.className.includes('is-truncated')).toEqual(true)
+    const wrapper = mount(<Text truncated />)
+    expect(wrapper.html()).toContain('is-truncated')
+  })
+
+  test('line-clamp', () => {
+    const wrapper = mount(<Text line-clamp="2" />)
+    expect(wrapper.html()).toContain('is-line-clamp')
   })
 
   test('tag', () => {
-    const wrapper = mount(() => <Text tag="del" />)
-    const vm = wrapper.vm
-    expect(vm.$el.tagName).toEqual('DEL')
+    const wrapper = mount(<Text tag="del" />)
+    expect(wrapper.html()).toContain('</del>')
+  })
+
+  test('default slot', () => {
+    const wrapper = mount(
+      <Text
+        v-slots={{
+          default: () => AXIOM,
+        }}
+      />
+    )
+    expect(wrapper.text()).toEqual(AXIOM)
+  })
+
+  test('tooltip slot', async () => {
+    mount(
+      <Text
+        v-slots={{
+          tooltip: () => AXIOM,
+        }}
+      />
+    )
+    expect(document.querySelector('body')?.innerHTML).toContain(AXIOM)
+  })
+
+  test('ellipsis event', async () => {
+    // TODO: jsdom not support `ResizeObserver`.
   })
 })
