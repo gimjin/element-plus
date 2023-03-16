de786f2ff3cc935059f266fdf1b2c2aa0b522fa1
gimjin <17204288@qq.com>
2023-03-16 13:47:21 +0800

    docs: examples span tag replace el-text, optimize div tags
    * replace div and span with EP components, the effect of the web page remains the same
    * clean up styles with unocss, the effect of the web page remains the same
    * only modify the example vue files
    * script tag total use <script lang="ts" setup>
    * style tag total use <style scoped>
    * vue file tag order is adjusted to <template> <script> <style>

--- docs/examples/affix/target.vue
+++ docs/examples/affix/target.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="affix-container">
+  <el-container class="affix-container">
     <el-affix target=".affix-container" :offset="80">
       <el-button type="primary">Target container</el-button>
     </el-affix>
-  </div>
+  </el-container>
 </template>
+
 <style scoped>
 .affix-container {
+  display: block;
   text-align: center;
   height: 400px;
   border-radius: 4px;
